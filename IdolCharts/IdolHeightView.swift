7b0f3fc9a85d22facb00e3e619203664cafcb776
BAN Jun <banjun@gmail.com>
2022-07-23 00:28:05 +0900

    add preview

--- IdolCharts/IdolHeightView.swift
+++ IdolCharts/IdolHeightView.swift
@@ -22,3 +22,16 @@ struct IdolHeightView: View {
 //        }
     }
 }
+
+struct IdolHeightView_Previews: PreviewProvider {
+    static var previews: some View {
+        IdolHeightView(idols: [
+            IdolHeight(name: "橘ありす", height: 141, color: "5881C1"),
+            IdolHeight(name: "橘ありす+10", height: 151, color: "666666"),
+            IdolHeight(name: "橘ありす+20", height: 161, color: "666666"),
+            IdolHeight(name: "橘ありす+30", height: 171, color: "666666"),
+            IdolHeight(name: "橘ありす+40", height: 181, color: "666666"),
+        ])
+        .padding()
+    }
+}
